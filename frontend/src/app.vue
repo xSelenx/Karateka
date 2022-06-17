95040bed36042228bc96cf7d9afcb91d4a7e44c3
Selen Oruc <selen.oruc@hotmail.com>
2022-06-17 11:15:45 +0200

    added navbar

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -14,11 +14,32 @@ export default {
 </script>
 <template lang="pug">
   #app
-    #nav
+    //#nav
       router-link(to="/profile") Profile
       router-link(to="/login") Login
       router-link(to="/register") Register
       a(@click="doLogout" href="#") Logout
+    nav#nav.navbar.navbar-expand-lg.bg-light
+      .container-fluid
+        button.navbar-toggler(type='button', data-bs-toggle='collapse', data-bs-target='#navbarTogglerDemo01', aria-controls='navbarTogglerDemo01', aria-expanded='false', aria-label='Toggle navigation')
+          span.navbar-toggler-icon
+        #navbarTogglerDemo01.collapse.navbar-collapse
+          router-link.navbar-brand(to='/') Karateka
+          ul.navbar-nav.me-auto.mb-2.mb-lg-0
+            li.nav-item
+            router-link(to="/profile") Profile
+              //a.nav-link.active(aria-current='page', href='#') Home
+            li.nav-item
+            router-link(to="/login") Login
+              //a.nav-link(href='#') Link
+            li.nav-item
+            router-link(to="/register") Register
+              //a.nav-link.disabled Disabled
+            li.nav-item
+            a.nav-link(a(@click="doLogout" href="#") Logout)
+          form.d-flex(role='search')
+            input.form-control.me-2(type='search', placeholder='Search', aria-label='Search')
+            button.btn.btn-outline-success(type='submit') Search
     router-view
 </template>
 
@@ -31,7 +52,7 @@ export default {
 }
 
 #nav {
-  padding: 30px;
+  padding: 16px;
 
   a {
     font-weight: bold;
