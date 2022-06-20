e6736bd2e39ede41c2d02f39cd14d29b7d7622c3
Selen Oruc <selen.oruc@hotmail.com>
2022-06-20 14:26:02 +0200

    post message routes

--- backend/src/routes/messages.js
+++ backend/src/routes/messages.js
@@ -36,6 +36,21 @@ router.get('/init', async (req, res) => {
   })
 
   console.log(message1)
+  message1.subject = 'Anything'
+  await message1.save()
+  console.log(message1)
+
+  const message2 = await Message.create({
+    message: 'Hello',
+    subject: 'Friday',
+    sender: 'selen.oruc@hotmail.com',
+    recipient: ['steve@coyotiv.com'],
+  })
+
+  console.log(message2)
+  message2.subject = 'something else'
+  await message2.save()
+  console.log(message2)
   //
   //    Mongoose/mongo give us a save() function!
   //
@@ -69,5 +84,9 @@ router.delete('/:userId', async (req, res) => {
   await Message.deleteOne()
   res.send()
 })
-
+// Post a new message
+router.post('/', async (req, res) => {
+  const createdUser = await Message.create(req.body)
+  res.send(createdUser)
+})
 module.exports = router
